a451889c33a84ed208c1c62e9085a0347d80c006
Wendalf <guangw.xu@gmail.com>
2016-08-02 13:34:52 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,15 +16,13 @@ class Transfer
 
   def execute_transaction
   	# binding.pry
-  	if self.status == "pending"
+  	if self.valid? && self.sender.balance > amount && self.status == "pending"
   		self.sender.balance -= self.amount
   		self.receiver.balance += self.amount
-  		if self.valid?
-  			self.status = "complete"
-  		else
-  			self.status = "rejected"
-  			"Transaction rejected. Please check your account balance."
-  		end
+  		self.status = "complete"
+  	else
+		self.status = "rejected"
+		"Transaction rejected. Please check your account balance."
   	end
   end
 
